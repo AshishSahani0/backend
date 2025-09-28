f4896ddb6cceaa2cf4368e8543c8042e1cbc1e90
Ashish Sahani <ashishsahani0011@gmail.com>
2025-09-28 20:24:25 +0530

    Update sendToken.js

--- util/sendToken.js
+++ util/sendToken.js
@@ -10,7 +10,7 @@ export const sendToken = (user, statusCode, message, res) => {
       expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure:process.env.NODE_ENV === "production",
-      sameSite: "None",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       path: "/",
     };
 
